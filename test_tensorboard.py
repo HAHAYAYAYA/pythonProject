57d8c809127b3a30e2e392e7fc93cac4ff6a93b1
haha <hahayayaxu@gmail.com>
2024-05-23 11:15:00 +0800

    第一次提交

--- test_tensorboard.py
+++ test_tensorboard.py
@@ -4,5 +4,9 @@ from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter('logs')
 
 # writer.add_image('image', torch.rand(3, 3, 3, 3))
-writer.add_scalar('scalar', torch.rand(3, 3, 3, 3))
+#y = x
+for i in range(100):
+    writer.add_scalar("y=2x", 2*i, i)
+
+
 writer.close()
